b7ab0b80e094ab71aad8275a4c78c4a0d22505a8
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-03-14 20:37:47 -0500

    removed attacks and enemies from db schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,24 +3,6 @@ CREATE DATABASE fitness_db;
 
 USE fitness_db;
 
-DROP TABLE IF EXISTS attack;
-CREATE TABLE attack (
-  attack_id int NOT NULL,
-  category varchar(25),
-  activity varchar(25),
-  damage int,
-  PRIMARY KEY (attack_id)
-);
-
-
-DROP TABLE IF EXISTS enemy;
-CREATE TABLE enemy (
-  enemy_id int NOT NULL,
-  name varchar(25) DEFAULT NULL,
-  health int DEFAULT NULL,
-  PRIMARY KEY (enemy_id)
-);
-
 DROP TABLE IF EXISTS fight;
 CREATE TABLE fight (
   fight_id int NOT NULL,
