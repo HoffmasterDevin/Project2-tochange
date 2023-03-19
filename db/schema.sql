15754bd8c9a73674412caad0ccec1edd7e8f930a
HoffmasterDevin <118146567+HoffmasterDevin@users.noreply.github.com>
2023-03-19 09:08:22 -0500

    minor fixes

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,4 @@
 DROP DATABASE IF EXISTS fitness_db;
 CREATE DATABASE fitness_db;
 
-USE fitness_db;
 
-DROP TABLE IF EXISTS fight;
-CREATE TABLE fight (
-  fight_id int NOT NULL,
-  user_id int NOT NULL,
-  date datetime NOT NULL,
-  enemy_id int NOT NULL,
-  defeated tinyint NOT NULL,
-  PRIMARY KEY (fight_id)
-);
-
-
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id int NOT NULL,
-  username varchar(16) NOT NULL,
-  email varchar(255) DEFAULT NULL,
-  password varchar(32) NOT NULL,
-  PRIMARY KEY (user_id)
-);
